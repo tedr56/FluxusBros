a728be3bf3d392bef86e8a332815f806b2939ab2
VJ T <ted.r@hotmail.fr>
2012-12-10 16:20:51 +0100

    Specify current directory in Launcher

--- ControlMapperLauncher.scm
+++ ControlMapperLauncher.scm
@@ -1,1 +1,7 @@
-(load "ControlMapper.scm") 
+;::: specify the FluxusBros root directory
+(current-directory "/home/ted/Sources/git/FluxusBros")
+(define debug #f)
+
+(load "ControlMapper.scm")
+
+(show-fps 0)
