a069f4e8b3f988f67faaafed6ebb3af24d23e82e
Adrien Rocher Vj <a.rocher.vj@gmail.com>
2016-04-22 18:38:18 +0200

    indent

--- source/controlmapper.scm
+++ source/controlmapper.scm
@@ -24,11 +24,11 @@
             (last-osc-peek "no message yet...")
         )
         (define/public (getTypesEvents)
-      (show TypesEvents)
-    )
-    (define/public (get-players)
-      Players
-    )
+            (show TypesEvents)
+        )
+        (define/public (get-players)
+          Players
+        )
         (define/public (recordControl table-filterControls) ; table-filterControls '(TableControl% FilterControl%))
             (show-d "->recordControl ")
             (for-each
